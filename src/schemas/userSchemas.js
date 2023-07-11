034315e9edeaa9ba136ee9a74e816b97374cc657
Lucas Casal <lucasocteixeira@gmail.com>
2023-07-11 18:58:15 -0300

    deploy

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -1,12 +1,12 @@
 import Joi from "joi";
 
 export const loginSchema = Joi.object({
-    email:Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email:Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['br', 'com', 'net'] } }).required(),
     password:Joi.string().min(3).required()
 })
 
 export const signUpSchema = Joi.object({
     name:Joi.string().required(),
-    email:Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    email:Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['br', 'com', 'net'] } }).required(),
     password:Joi.string().min(3).required()
 })
